3e7eca6f03b6953f29397db4d47ac7dc52d69b59
Kazmir-Raiyan <kazmirr003@gmail.com>
2025-11-01 16:35:51 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Tic_tac_toe
+{
+    class Program
+    {
+     /* 1 | 2 | 3
+        4 | 5 | 6
+        7 | 8 | 9 */ 
+        static void Main()
+        {
+            Game tictactoe = new();
+            int counter = 0;
+            tictactoe.Intro();
+            while (tictactoe.Win("X") == false || tictactoe.Win("O") == false || counter < 9) 
+            {
+                var inputOne = tictactoe.askData("X");
+                tictactoe.board[inputOne] = "X";
+                Console.WriteLine($"You played X on {inputOne + 1}!");
+                tictactoe.drawBoard();
+                counter++;
+                if (tictactoe.Win("X") == true)
+                {
+                    Console.WriteLine("Congratulation! Player X Won!!");
+                    break;
+                }
+                if (counter == 9)
+                {
+                    Console.WriteLine("The game was a tie and there were no winners.");
+                    break;
+                }
+                var inputTwo = tictactoe.askData("O");
+                tictactoe.board[inputTwo] = "O";
+                Console.WriteLine($"You played O on {inputTwo + 1}!");
+                tictactoe.drawBoard();
+                counter++;
+                if (tictactoe.Win("O") == true)
+                {
+                    Console.WriteLine("Congratulation! Player O Won!!");
+                    break;
+                }
+            }
+
+        }
+    }
+}
---
+++ pack.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tic_tac_toe
+{
+    public class Game
+    {
+        public string[] board = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+        public void Intro() 
+        {
+            Console.WriteLine("Welcome to Tic Tac Toe");
+            Console.Write("Press any key to continue. ");
+            Console.ReadLine();
+        } 
+        public bool Win(string player) 
+        {
+            if (board[0] == player && board[1] == player && board[2] == player)
+                return true;
+            if (board[3] == player && board[4] == player && board[5] == player)
+                return true;
+            if (board[6] == player && board[7] == player && board[8] == player)
+                return true;
+            if (board[0] == player && board[3] == player && board[6] == player)
+                return true;
+            if (board[1] == player && board[4] == player && board[7] == player)
+                return true;
+            if (board[2] == player && board[5] == player && board[8] == player)
+                return true;
+            if (board[0] == player && board[4] == player && board[8] == player)
+                return true;
+            if (board[2] == player && board[4] == player && board[6] == player)
+                return true;
+            return false;
+        }
+        public void drawBoard() 
+        {           
+             Console.WriteLine($"{board[0]} | {board[1]} | {board[2]}");    
+             Console.WriteLine($"{board[3]} | {board[4]} | {board[5]}");
+             Console.WriteLine($"{board[6]} | {board[7]} | {board[8]}");
+        }
+        public int askData(string player)
+        {
+            Console.WriteLine($"PLayer: {player}");
+            drawBoard();
+            Console.Write("Please enter your selection: ");
+            bool success = int.TryParse(Console.ReadLine(), out int input);
+            while (success != true || input <= 0 || input > 9 || board[input - 1] == "X" || board[input - 1] == "O") 
+            {
+                Console.WriteLine("Invalid input. Please try again.");
+                Console.WriteLine($"PLayer: {player}");
+                Console.Write("Please enter your selection: ");
+                success = int.TryParse(Console.ReadLine(), out input);
+            }
+            return input - 1;            
+        }   
+    }
+}
